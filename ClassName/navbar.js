32bc70cdac8c2c53c7b9d26f796099616aa3f284
dedisalam <dedispermana20@gmail.com>
2022-03-24 14:46:52 +0700

    update 24032022 14:46

---
+++ ClassName/navbar.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+export const NAVBAR_EXPAND = {
+  sm: 'navbar-expand-sm',
+  md: 'navbar-expand-md',
+  lg: 'navbar-expand-lg',
+  xl: 'navbar-expand-xl',
+  xxl: 'navbar-expand-xxl',
+  none: 'navbar-expand',
+};
